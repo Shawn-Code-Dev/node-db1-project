be5a541042595321af2e4e79c9a1704e6b45cf89
Shawn Rice <Shawn.CS@protonmail.com>
2021-10-05 08:07:36 -0400

    I give up

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,57 @@
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+const Accounts = require('./accounts-model')
+const yup = require('yup')
+const db = require('../../data/db-config')
+
+exports.checkAccountPayload = async (req, res, next) => {
+  try {
+    const account = await accSchema.validate(req.body)
+    req.body = account
+    next()
+  } catch (err) {
+    next({ status: 400, message: err.message })
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const notUnique = await db('accounts').where('name', req.body.name.trim()).first()
+
+    if (notUnique) {
+      next({ status: 400, message: 'that name is taken'})
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const acc = await Accounts.getById(req.params.id)
+    if (!acc) {
+      next({ status: 404, message: 'account not found'})
+    } else {
+      req.acc = acc
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
+
+const accSchema = yup.object().shape({
+  name: yup
+    .string()
+    .typeError('name of account must be a string')
+    .trim()
+    .min(3, 'name of account must be between 3 and 100')
+    .max(100, 'name of account must be between 3 and 100')
+    .required('name and budget are required'),
+  budget: yup
+    .number()
+    .typeError('budget of account must be a number')
+    .min(0, 'budget of account is too large or too small')
+    .max(1000000, 'budget of account is too large or too small')
+    .required('name and budget are required')
+})
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,50 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
+const { checkAccountId, checkAccountPayload } = require('./accounts-middleware')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const accs = await Accounts.getAll()
+    res.json(accs)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, async (req, res) => {
+    res.json(req.acc)
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
+  try {
+    const acc = await Accounts.updateById(req.params.id, req.body)
+    res.json(acc)
+  } catch (err) {
+    next(err)
+  }
+})
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  try {
+    await Accounts.deleteById(req.params.id)
+    res.json(req.acc)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
